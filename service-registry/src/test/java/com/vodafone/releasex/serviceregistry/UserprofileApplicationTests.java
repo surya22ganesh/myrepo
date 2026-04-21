d75c51838dc18e64e00f7c74f2114d7d195e4092
Surya Rajendran <surya.rajendran@sandhata.com>
2026-04-21 15:07:09 +0530

    changes.

---
+++ service-registry/src/test/java/com/vodafone/releasex/serviceregistry/UserprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.releasex.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
